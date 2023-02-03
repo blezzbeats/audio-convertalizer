866e5166b93e46e2d82583e750c8f5acbf580bea
Mjuksel <mportma@gmail.com>
2023-02-03 17:48:07 +0100

    format everything, add comments to infer types

--- conv.js
+++ conv.js
@@ -1,4 +1,9 @@
-convertAudio = function (input,selectedFormat) {
+/**
+ * @param {string} input
+ * @param {string} selectedFormat
+ * @returns
+ */
+const convertAudio = function (input, selectedFormat) {
 	let segments = input.split('/');
 	let filename = segments[segments.length - 1];
 	let extension = filename.split('.')[1];
@@ -8,10 +13,10 @@ convertAudio = function (input,selectedFormat) {
 	console.log(bvalue);
 	console.log(svalue);
 
-  let output = folder + name + '_conv' + '.' + selectedFormat;
-  console.log("\Converting file %s", output)
+	let output = folder + name + '_conv' + '.' + selectedFormat;
+	console.log("\Converting file %s", output)
 
-	if (selectedFormat == 'mp3'){
+	if (selectedFormat == 'mp3') {
 		var command = ffmpeg(input)
 			.inputFormat(extension)
 			.audioCodec('libmp3lame')
@@ -20,28 +25,25 @@ convertAudio = function (input,selectedFormat) {
 			.audioFrequency(svalue)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'wav' && bvalue != 8){
+	} else if (selectedFormat == 'wav' && bvalue != 8) {
 		var command = ffmpeg(input)
 			.inputFormat(extension)
-			.audioCodec('pcm_s'+bvalue+'le')
+			.audioCodec('pcm_s' + bvalue + 'le')
 			.format(selectedFormat)
 			.audioBitrate(bvalue)
 			.audioFrequency(svalue)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'wav' && bvalue == 8){
+	} else if (selectedFormat == 'wav' && bvalue == 8) {
 		var command = ffmpeg(input)
 			.inputFormat(extension)
-			.audioCodec('pcm_u'+bvalue)
+			.audioCodec('pcm_u' + bvalue)
 			.audioBitrate(bvalue)
 			.audioFrequency(svalue)
 			.format(selectedFormat)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'raw'){
+	} else if (selectedFormat == 'raw') {
 		let output = folder + name + '_conv' + selectedFormat;
 		var command = ffmpeg(input)
 			.inputFormat(extension)
@@ -51,19 +53,17 @@ convertAudio = function (input,selectedFormat) {
 			.format(selectedFormat)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'aiff'){
+	} else if (selectedFormat == 'aiff') {
 		let output = folder + name + '_conv' + '.' + selectedFormat;
 		var command = ffmpeg(input)
 			.inputFormat(extension)
-			.audioCodec('pcm_s'+bvalue+'be')
+			.audioCodec('pcm_s' + bvalue + 'be')
 			.audioBitrate(bvalue)
 			.audioFrequency(svalue)
 			.format(selectedFormat)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'ogg'){
+	} else if (selectedFormat == 'ogg') {
 		let output = folder + name + '_conv' + '.' + selectedFormat;
 		var command = ffmpeg(input)
 			.inputFormat(extension)
@@ -73,8 +73,7 @@ convertAudio = function (input,selectedFormat) {
 			.format(selectedFormat)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'flac'){
+	} else if (selectedFormat == 'flac') {
 		let output = folder + name + '_conv' + '.' + selectedFormat;
 		var command = ffmpeg(input)
 			.inputFormat(extension)
@@ -84,8 +83,7 @@ convertAudio = function (input,selectedFormat) {
 			.format(selectedFormat)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'aac'){
+	} else if (selectedFormat == 'aac') {
 		let output = folder + name + '_conv' + '.' + selectedFormat;
 		var command = ffmpeg(input)
 			.inputFormat(extension)
@@ -95,8 +93,7 @@ convertAudio = function (input,selectedFormat) {
 			.format(selectedFormat)
 			.save(output)
 		return output;
-	}
-	else if (selectedFormat == 'ac3'){
+	} else if (selectedFormat == 'ac3') {
 		let output = folder + name + '_conv' + '.' + 'mp3';
 		var command = ffmpeg(input)
 			.inputFormat(extension)
@@ -107,49 +104,46 @@ convertAudio = function (input,selectedFormat) {
 			.save(output)
 		return output;
 	}
-
 }
 
-
 document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-  const dropZoneElement = inputElement.closest(".drop-zone");
+	const dropZoneElement = inputElement.closest(".drop-zone");
 
-  dropZoneElement.addEventListener("dragover", e => {
+	dropZoneElement.addEventListener("dragover", e => {
 		e.preventDefault();
-    dropZoneElement.classList.add("drop-zone--over");
+		dropZoneElement.classList.add("drop-zone--over");
 	});
 
 	["dragleave", "dragend"].forEach(type => {
-		dropZoneElement.addEventListener(type, e=> {
+		dropZoneElement.addEventListener(type, e => {
 			dropZoneElement.classList.remove('drop-zone--over');
 			dropZoneElement.classList.add("drop-zone");
-			});
 		});
+	});
 
-		dropZoneElement.addEventListener("drop", e => {
-			e.preventDefault();
-			dropZoneElement.classList.remove('drop-zone--over');
-			dropZoneElement.classList.add("drop-zone");
+	dropZoneElement.addEventListener("drop", e => {
+		e.preventDefault();
+		dropZoneElement.classList.remove('drop-zone--over');
+		dropZoneElement.classList.add("drop-zone");
 
-			if (e.dataTransfer.files.length) {
-				inputElement.files = e.dataTransfer.files;
-        console.log(inputElement.files)
-				console.log(e.dataTransfer.files[0].path);
-        console.log(e.dataTransfer.files.length);
-        let audiofiles = e.dataTransfer.files;
-        //console.log(e.dataTransfer.files[1].path);
-        //convertAudio(e.dataTransfer.files[0].path)
+		if (e.dataTransfer.files.length) {
+			inputElement.files = e.dataTransfer.files;
+			console.log(inputElement.files)
+			console.log(e.dataTransfer.files[0].path);
+			console.log(e.dataTransfer.files.length);
+			let audiofiles = e.dataTransfer.files;
+			//console.log(e.dataTransfer.files[1].path);
+			//convertAudio(e.dataTransfer.files[0].path)
 
-        for(i=0;i < e.dataTransfer.files.length;i++)
-          {
-          console.log("Converting "+e.dataTransfer.files[i].path);
-          convertAudio(e.dataTransfer.files[i].path,format);
-          }
-
-				//updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+			for (i = 0; i < e.dataTransfer.files.length; i++) {
+				console.log("Converting " + e.dataTransfer.files[i].path);
+				convertAudio(e.dataTransfer.files[i].path, format);
 			}
-	    //console.log(e);
-		});
+
+			//updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+		}
+		//console.log(e);
+	});
 
 });
 
@@ -167,95 +161,87 @@ var format = "wav";
 
 var button = document.getElementsByClassName("button");
 
-		var addSelectClass = function(){
-			removeSelectClass();
-			this.classList.add('selected');
-		}
+var addSelectClass = function () {
+	removeSelectClass();
+	this.classList.add('selected');
+}
 
-		var removeSelectClass = function(){
-			for (var i =0; i < button.length; i++) {
-				button[i].classList.remove('selected')
-			}
-		}
+var removeSelectClass = function () {
+	for (var i = 0; i < button.length; i++) {
+		button[i].classList.remove('selected')
+	}
+}
 
-		var updateSelection = function(){
-			console.log("selected.");
-			console.log(button[0]);
-			if (this.innerText === "WAV"){
-				format = "wav";
-				bvalue = 16;
-				svalue = 44100;
-				console.log(format);
-				bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
-				sampler.style.display = "block";
-				sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
-			}
-			else if (this.innerText === "MP3"){
-				format = "mp3";
-				bvalue = 96;
-				console.log(format);
-				bitr.innerHTML = '<option value="96k">96 kbps</option><option value="128k">128 kbps</option><option value="256k">256 kbps</option><option value="320k">320 kbps</option>'
-				sampler.style.display = "none";
-			}
-			else if (this.innerText === "AIFF"){
-				format = "aiff";
-				bvalue = 16;
-				svalue = 44100;
-				console.log(format);
-				bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option>'
-				sampler.style.display = "block";
-				sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
-			}
-			else if (this.innerText === "OGG"){
-				format = "ogg";
-				bvalue = 96;
-				console.log(format);
-				bitr.innerHTML = '<option value="96k">96 kbps</option><option value="128k">128 kbps</option><option value="256k">256 kbps</option><option value="320k">320 kbps</option>'
-				sampler.style.display = "none";
-			}
-			else if (this.innerText === "FLAC"){
-				format = "flac";
-				bvalue = 16;
-				svalue = 44100;
-				console.log(format);
-				sampler.style.display = "block";
-				bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
-				sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
+var updateSelection = function () {
+	console.log("selected.");
+	console.log(button[0]);
+	if (this.innerText === "WAV") {
+		format = "wav";
+		bvalue = 16;
+		svalue = 44100;
+		console.log(format);
+		bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
+		sampler.style.display = "block";
+		sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
+	} else if (this.innerText === "MP3") {
+		format = "mp3";
+		bvalue = 96;
+		console.log(format);
+		bitr.innerHTML = '<option value="96k">96 kbps</option><option value="128k">128 kbps</option><option value="256k">256 kbps</option><option value="320k">320 kbps</option>'
+		sampler.style.display = "none";
+	} else if (this.innerText === "AIFF") {
+		format = "aiff";
+		bvalue = 16;
+		svalue = 44100;
+		console.log(format);
+		bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option>'
+		sampler.style.display = "block";
+		sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
+	} else if (this.innerText === "OGG") {
+		format = "ogg";
+		bvalue = 96;
+		console.log(format);
+		bitr.innerHTML = '<option value="96k">96 kbps</option><option value="128k">128 kbps</option><option value="256k">256 kbps</option><option value="320k">320 kbps</option>'
+		sampler.style.display = "none";
+	} else if (this.innerText === "FLAC") {
+		format = "flac";
+		bvalue = 16;
+		svalue = 44100;
+		console.log(format);
+		sampler.style.display = "block";
+		bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
+		sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
+	} else if (this.innerText === "RAW") {
+		format = "raw";
+		bvalue = 16;
+		svalue = 44100;
+		console.log(format);
+		bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
+		sampler.style.display = "block";
+		sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
+	} else if (this.innerText === "AAC") {
+		format = "aac";
+		bvalue = 16;
+		svalue = 44100;
+		console.log(format);
+		bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
+		sampler.style.display = "block";
+		sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
+	} else if (this.innerText === "AC3") {
+		format = "ac3";
+		bvalue = 96;
+		console.log(format);
+		bitr.innerHTML = '<option value="96k">96 kbps</option><option value="128k">128 kbps</option><option value="256k">256 kbps</option><option value="320k">320 kbps</option>'
+		sampler.style.display = "none";
+	}
 
-			}
-			else if (this.innerText === "RAW"){
-				format = "raw";
-				bvalue = 16;
-				svalue = 44100;
-				console.log(format);
-				bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
-				sampler.style.display = "block";
-				sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
-			}
-			else if (this.innerText === "AAC"){
-				format = "aac";
-				bvalue = 16;
-				svalue = 44100;
-				console.log(format);
-				bitr.innerHTML = '<option value="16">16-bit</option><option value="24">24-bit</option><option value="32">32-bit</option><option value="8">8-bit</option>'
-				sampler.style.display = "block";
-				sampler.innerHTML = '<option value="44100">44100 kHz</option><option value="48000">48000 kHz</option><option value="22050">22050 kHz</option><option value="16000">16000 kHz</option><option value="11025">11025 kHz</option><option value="8000">8000 kHz</option><option value="88200">88200 kHz</option><option value="96000">96000 kHz</option><option value="192000">192000 kHz</option>'
-			}
-			else if (this.innerText === "AC3"){
-				format = "ac3";
-				bvalue = 96;
-				console.log(format);
-				bitr.innerHTML = '<option value="96k">96 kbps</option><option value="128k">128 kbps</option><option value="256k">256 kbps</option><option value="320k">320 kbps</option>'
-				sampler.style.display = "none";
-			}
+	//button[0].innerText
+}
 
-			//button[0].innerText
-		}
-
-		for (var i =0; i < button.length; i++) {
-			button[i].addEventListener("click",addSelectClass);
-			button[i].addEventListener("click",updateSelection);
-		}
+for (var i = 0; i < button.length; i++) {
+	button[i].addEventListener("click", addSelectClass);
+	button[i].addEventListener("click", updateSelection);
+}
 
 //QUALITY SELECTION
 var bitr = document.getElementById("bitrates");
@@ -267,15 +253,15 @@ var svalue = sampler.value;
 var stext = sampler.options[sampler.selectedIndex].text;
 
 function bitChange() {
-  bvalue = bitr.value;
-  btext = bitr.options[bitr.selectedIndex].text;
-  console.log(bvalue, btext);
+	bvalue = bitr.value;
+	btext = bitr.options[bitr.selectedIndex].text;
+	console.log(bvalue, btext);
 }
 
 function rateChange() {
-  svalue = sampler.value;
-  stext = sampler.options[sampler.selectedIndex].text;
-  console.log(svalue, stext);
+	svalue = sampler.value;
+	stext = sampler.options[sampler.selectedIndex].text;
+	console.log(svalue, stext);
 }
 
 bitr.onchange = bitChange;
--- main.js
+++ main.js
@@ -7,53 +7,53 @@ const dropzone = require('dropzone')
 const ffmpeg = require('fluent-ffmpeg');
 //Get the paths to the packaged versions of the binaries we want to use
 const ffmpegPath = require('ffmpeg-static').replace(
-    'app.asar',
-    'app.asar.unpacked'
+	'app.asar',
+	'app.asar.unpacked'
 );
 const ffprobePath = require('ffprobe-static').path.replace(
-    'app.asar',
-    'app.asar.unpacked'
+	'app.asar',
+	'app.asar.unpacked'
 );
 //tell the ffmpeg package where it can find the needed binaries.
 ffmpeg.setFfmpegPath(ffmpegPath);
 ffmpeg.setFfprobePath(ffprobePath);
 
 const createWindow = () => {
-  var win = new BrowserWindow({
-    width: 220,
-    height: 365,
-    title: "Audio Convertalizer",
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    nodeIntegration: true,
-    contextIsolation: false,
-    frame: true,
-    resizable: true,
-    transparent: true
-    },
-  })
+	var win = new BrowserWindow({
+		width: 220,
+		height: 365,
+		title: "Audio Convertalizer",
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+			contextIsolation: false,
+			frame: true,
+			resizable: true,
+			transparent: true
+		},
+	})
 
-  win.loadFile('index.html')
-  //win.removeMenu()
-  //win.setResizable(false)
+	win.loadFile('index.html')
+	//win.removeMenu()
+	//win.setResizable(false)
 
-  win.on('closed', () => {
-    win = null
-  })
+	win.on('closed', () => {
+		win = null
+	})
 }
 
 app.whenReady().then(() => {
-  createWindow()
+	createWindow()
 
-  app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 })
 
 app.on('window-all-closed', () => {
   /*if (process.platform !== 'darwin')*/ {
-    app.quit()
-  }
+		app.quit()
+	}
 })
